6f5a87b159d7ca0a0f224c3f3a93b10d410c3edd
Natalie Akam <natalie.akam@gmail.com>
2015-08-14 16:15:37 +0100

    Added test for view changes

--- test/e2e/views.spec.js
+++ test/e2e/views.spec.js
@@ -1,5 +1,8 @@
 describe('Changing views', function() {
 
+  var homeNav = element(by.className('tab-nav')).element(by.className('ion-images'));
+ // var homeNav = element(by.xpath('/html/body/ion-nav-view/ion-tabs/div/a[1]/i'));
+
   beforeEach(function() {
     browser.get('http://192.168.50.174:8100');
   });
@@ -9,6 +12,11 @@ describe('Changing views', function() {
   });
 
   it('should start on Homepage', function() {
-    expect(element(by.css('ion-nav-view')).getText()).toContain('Home')
-  })
-})
+    expect(element(by.css('ion-nav-bar')).getText()).toContain('iSpy London');
+  });
+
+  it('navigates to Photo Album', function() {
+    homeNav.click();
+    expect(element(by.css('ion-nav-bar')).getText()).toContain('Photo Album');
+  });
+});
