9d907bd9dacb0b617205d7f8f2be483da702657e
Christian Miller <cwgmiller@gmail.com>
2015-08-14 12:39:18 +0100

    Popover example added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,12 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope) {})
+.controller('HomeCtrl', function($scope, $ionicPopover) {
+  $ionicPopover.fromTemplateUrl('templates/cluepopover.html', {
+    scope: $scope,
+  }).then(function(popover) {
+    $scope.popover = popover;
+  });
+})
+
 
 .controller('PhotoAlbumCtrl', function($scope) {});
