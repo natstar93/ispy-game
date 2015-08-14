533297e3dac9161c16d3f098c9edf76e95ad980e
Natalie Akam <natalie.akam@gmail.com>
2015-08-14 16:20:52 +0100

    Added files for view testing

--- test/e2e/views.spec.js
+++ test/e2e/views.spec.js
@@ -1,8 +1,7 @@
 describe('Changing views', function() {
 
   var homeNav = element(by.className('tab-nav')).element(by.className('ion-images'));
- // var homeNav = element(by.xpath('/html/body/ion-nav-view/ion-tabs/div/a[1]/i'));
-
+ 
   beforeEach(function() {
     browser.get('http://192.168.50.174:8100');
   });
