82491cee3febf021b3b4f239d607cde87a6ac96b
Christian Miller <cwgmiller@gmail.com>
2015-08-17 17:49:23 +0100

    View rendering in modal

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,33 @@
 angular.module('starter.controllers', [])
 
 .controller('HomeCtrl', function($scope, $ionicModal) {
-  $ionicModal.fromTemplateUrl('templates/cluepopover.html', {
+  $ionicModal.fromTemplateUrl('templates/cluemodal.html', {
     scope: $scope,
     animation: 'slide-in-up'
   }).then(function(modal) {
     $scope.modal = modal;
   });
-  $scope.openModal = function() {
+
+  var monuments = {
+    shard: {name: 'The Shard',
+            clue1: 'Its pointy',
+            clue2: 'TBC',
+            clue3: 'TBC',
+            image: 'To add image',
+            map: 'To add map'
+            },
+    bigben: {name: 'Big Ben',
+            clue1: 'Dong',
+            clue2: 'TBC',
+            clue3: 'TBC',
+            image: 'To add image',
+            map: 'To add map'
+            }
+
+    };
+
+  $scope.openModal = function(name) {
+    $scope.selectedname = monuments[name];
     $scope.modal.show();
   };
   $scope.closeModal = function() {
@@ -25,7 +45,9 @@ angular.module('starter.controllers', [])
   $scope.$on('modal.removed', function() {
     // Execute action
   });
-})
 
+
+
+})
 
 .controller('PhotoAlbumCtrl', function($scope) {});
