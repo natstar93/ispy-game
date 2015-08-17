50fe1fbd8346e5b2d40f210152d0e36da15dbc93
DuskyShelf <dupsdale@gmail.com>
2015-08-17 14:52:42 +0100

    working camera and photo gallery

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,37 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope) {})
+.controller('HomeCtrl', function($scope, ImageService, $state, $cordovaCamera, $cordovaFile) {
+  $scope.imageService = ImageService;
+})
 
-.controller('PhotoAlbumCtrl', function($scope) {});
+.controller('PhotoAlbumCtrl', function($scope, ImageService, $cordovaCamera, $cordovaFile) {
+  $scope.imageService = ImageService;
+})
+
+.service('ImageService', function($state, $cordovaCamera, $cordovaFile) {
+
+  var self = this;
+
+  self.images = [];
+
+  self.addImage = function(photoIndex) {
+
+    console.log('hello addImage()');
+
+    var options = {
+      destinationType : Camera.DestinationType.FILE_URI,
+      sourceType : Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
+      allowEdit : false,
+      encodingType: Camera.EncodingType.JPEG,
+      popoverOptions: CameraPopoverOptions,
+    };
+
+    $cordovaCamera.getPicture(options).then(function(imageData) {
+
+      self.images[photoIndex] = {url: imageData};
+
+    }, function(err) {
+      console.log(err);
+    });
+  };
+});
