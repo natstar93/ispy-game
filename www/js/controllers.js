d7983ac665c516d21f31eb0d4fa1f7d43f0c9353
Christian Miller <cwgmiller@gmail.com>
2015-08-17 15:18:38 +0100

    Modal changed instead of Popover

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,31 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope, $ionicPopover) {
-  $ionicPopover.fromTemplateUrl('templates/cluepopover.html', {
+.controller('HomeCtrl', function($scope, $ionicModal) {
+  $ionicModal.fromTemplateUrl('templates/cluepopover.html', {
     scope: $scope,
-  }).then(function(popover) {
-    $scope.popover = popover;
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
   });
 })
+
 
 .controller('PhotoAlbumCtrl', function($scope) {});
