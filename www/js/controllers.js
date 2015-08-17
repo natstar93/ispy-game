2d19e35ee2a2d51e4de81f5d010ffa16d5c63ba9
DuskyShelf <dupsdale@gmail.com>
2015-08-17 18:25:08 +0100

    refactored angular services

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['starter.services'])
 
 .controller('HomeCtrl', function($scope, ImageService, $state, $cordovaCamera, $cordovaFile) {
   $scope.imageService = ImageService;
@@ -6,46 +6,4 @@ angular.module('starter.controllers', [])
 
 .controller('PhotoAlbumCtrl', function($scope, ImageService, $cordovaCamera, $cordovaFile) {
   $scope.imageService = ImageService;
-})
-
-.service('ImageService', function($state, $cordovaCamera, $cordovaFile) {
-
-  var self = this;
-  self.images = JSON.parse(window.localStorage.images || '[]');
-
-  self.placeheldgallery = [];
-
-  for(var i=0; i < 12; i++) {
-    if(self.images[i]) {
-      self.placeheldgallery[i] = self.images[i];
-    }
-    else {
-      self.placeheldgallery[i] = {url: "img/pigeonplaceholder.gif"};
-    }
-  }
-
-  self.addImage = function(photoIndex) {
-
-    console.log('hello addImage()');
-
-    var options = {
-      destinationType : Camera.DestinationType.FILE_URI,
-      sourceType : Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
-      allowEdit : false,
-      encodingType: Camera.EncodingType.JPEG,
-      popoverOptions: CameraPopoverOptions,
-    };
-
-    $cordovaCamera.getPicture(options).then(function(imageData) {
-
-      self.images[photoIndex] = {url: imageData};
-
-      self.placeheldgallery[photoIndex] = {url: imageData};
-
-      window.localStorage.images = JSON.stringify(self.images);
-
-    }, function(err) {
-      console.log(err);
-    });
-  };
 });
