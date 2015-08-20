8a344ef98556707ce0f8fc6f48d046d336867395
Christian Miller <cwgmiller@gmail.com>
2015-08-20 13:36:51 +0100

    blank map view available

--- www/js/app.js
+++ www/js/app.js
@@ -58,6 +58,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
         controller: 'PhotoAlbumCtrl'
       }
     }
+  })
+
+  .state('tab.map', {
+    url: '/map',
+    views: {
+      'tab-map': {
+        templateUrl: 'templates/tab-map.html',
+        controller: 'MapCtrl'
+      }
+    }
   });
 
   // if none of the above states are matched, use this as the fallback
